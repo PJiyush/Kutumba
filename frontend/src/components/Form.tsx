20b80aa562e500c1c9c16efde12ceb07130b939e
PJiyush <jhamnani.1@iitj.ac.in>
2023-06-24 09:07:58 +0530

    made the login Form functional

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 function Form() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(e);
+    }
+    
     return (
         <div className='flex justify-center mt-36'>
-            <form className='w-5/12 border-solid bg-pink-400 px-10 rounded py-5'>
+            <form className=' w-4/12  border-solid bg-pink-400 px-10 rounded py-5' onSubmit={handleSubmit} >
                 <div className="mb-3  bg-pink-400">
                     <label htmlFor="exampleInputEmail1" className="form-label  bg-pink-400 text-2xl cursor-pointer">Email address</label>
-                    <input type="email" className="border-2 bg-pink-200 text-gray-700 rounded-lg px-4 py-2 focus:outline-none form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" className="form-text  bg-pink-400">We'll never share your email with anyone else.</div>
+                    <input type="email" className="border-2 bg-pink-200 text-gray-700 rounded-lg px-4 py-2  focus:outline-none form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e)=>setEmail(e.target.value)} />
+
                 </div>
-                <div className="mb-3  bg-pink-400">
+                <div className="mb-3 mt-4  bg-pink-400">
                     <label htmlFor="exampleInputPassword1" className="form-label  bg-pink-400 text-2xl cursor-pointer">Password</label>
-                    <input type="password" className="form-control border-2 bg-pink-200 text-gray-700 rounded-lg px-4 py-2 focus:outline-none form-control" id="exampleInputPassword1"/>
+                    <input type="password" className="form-control border-2 bg-pink-200 text-gray-700 rounded-lg px-4 py-2 focus:outline-none form-control" id="exampleInputPassword1" value={password} onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <div className="mb-3 form-check bg-pink-400">
                     <input type="checkbox" className="form-check-input bg-pink-400 text-xl cursor-pointer" id="exampleCheck1"/>
@@ -18,6 +27,7 @@ function Form() {
                 </div>
                 <div className='h-20 bg-pink-400'>
                     {/* here login with google will come */}
+                    
                 </div>
                 <button type="submit" className="btn btn-primary bg-blue-600 flex mx-auto h-12 w-24  "><p className='text-xl bg-transparent flex mx-auto '>Log in</p></button>
             </form>
